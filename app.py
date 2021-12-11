80b1b1d3b4cc1e058e08006f996990f3eed1ded5
JasminQuast <72495754+JasminQuast@users.noreply.github.com>
2021-12-11 15:41:44 +0100

    HTML and CSS stuff

--- app.py
+++ app.py
@@ -2,17 +2,18 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/home/<name>')
-def name(name):
-    return render_template('home.html', name=name)
-
-@app.route('/post/<int:post_id>')
-def post(post_id):
-    return 'Post ID is %s' % post_id
+# def index():
+#     name= "Jasmin"
+#     return render_template("home.html", name=name)
 
 @app.route('/')
-def index():
-    return 'Method used: %s' % request.method
+def home():
+    return render_template('home.html')
+
+@app.route('/types')
+def types():
+    return render_template('types.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
